468b0ca7e5d0587650f102c757d0d15a1d78646f
Ivan Belyanin <belyanin-2001@list.ru>
2023-10-11 00:41:40 +0400

    Create project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="content">
+    <CardsList>
+
+    </CardsList>
+  </main>
+</template>
+
+<script>
+  import CardsList from '@/components/CardsList.vue';
+  export default {
+    components: {
+      CardsList,
+    }
+  }
+</script>
+
+<style scoped>
+  .main {
+    margin: 0 auto;
+    max-width: 1440px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/CardsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button class="button" type="button" @click="addCards">
+      Добавить карточки
+    </button>
+    <ul class="list" >
+      <li v-for="card in cards">
+        <p>{{ card.title }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name: 'CardsList',
+  data() {
+    return {
+      cards: [],
+    }
+  },
+  methods: {
+    addCards() {
+      axios.get(`https://fakestoreapi.com/products`)
+      .then(res => {
+        res.data.forEach(element => {
+            this.cards.push(element);
+        });
+      })
+      console.log(this.cards);
+    }
+  }
+}
+</script>
+<style scoped>
+    
+</style>
